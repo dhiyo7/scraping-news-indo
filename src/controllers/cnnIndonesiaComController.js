419bd9a69257f0555c376864ee156443303a5021
dhiyo7 <dhiyoreksa12@gmail.com>
2023-04-05 02:35:53 +0700

    [Dhiyo][FIX] - CNN news category & double data

--- src/controllers/cnnIndonesiaComController.js
+++ src/controllers/cnnIndonesiaComController.js
@@ -3,25 +3,33 @@ const cheerio = require("cheerio");
 const baseResponse = require('../helper/baseResponse')
 const articles = []
 const detailArticles = {}
+const urlList = [
+    {
+        name: 'news',
+        url: "https://www.cnnindonesia.com/indeks"
+    },
+]
 
-axios.get('https://www.cnnindonesia.com/indeks/')
-    .then((response) => {
-        const html = response.data
-        const chr = cheerio.load(html)
-        chr('article', html).each((index, element) => {
-            const title = chr(element).find('h2').text().trim()
-            const url = chr(element).find('a').attr('href')
-            const image = chr(element).find('img').attr('src')
-            const category = chr(element).find('.kanal').text().toLowerCase()
-            articles.push({
-                image,
-                title,
-                category,
-                url
+urlList.forEach(urlSingle => {
+    axios.get(urlSingle.url)
+        .then((response) => {
+            const html = response.data
+            const chr = cheerio.load(html)
+            chr('article', html).each((index, element) => {
+                const title = chr(element).find('h2').text().trim()
+                const url = chr(element).find('a').attr('href')
+                const image = chr(element).find('img').attr('src')
+                const category = chr(element).find('span.kanal').text().toLowerCase().replace(/\s/g,'')
+                articles.push({
+                    image,
+                    title,
+                    category,
+                    url
+                })
             })
-        })
+        }).catch((e) => console.log(e))
+})
 
-    }).catch((e) => console.log(e))
 module.exports = {
     getCnnNews: (req, res) => {
         const {category, search} = req.query
@@ -29,7 +37,7 @@ module.exports = {
             const reqCategory = articles.filter((a) => a?.category?.toLowerCase() === category?.toLowerCase() || a?.title?.toLowerCase().includes(search?.toLowerCase()))
             baseResponse.success(res, 'Success get data', 200, reqCategory)
         } else {
-            baseResponse.success(res, 'Success get data', 200, articles)
+            baseResponse.success(res, 'Success get data', 200, articles.slice(24, 35))
         }
     }
 }
